c9745b91c115af2cee43b2548203efdefef2b857
Dariusz Olszewski <dariusz.olszewski@mailfence.com>
2023-05-27 15:56:30 +0200

    start stop

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,31 +1,62 @@
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Demo {
-    static Stack<Project> projects = new Stack<>();
+    private List<Project> projects;
+
+    public Demo() {
+        this.projects = new ArrayList<>();
+    }
     static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-    public static void start(String projectName, String taskName) {
+
+    public void start(String projectName, String taskName) {
         Project newProject = new Project(projectName);
         Task newTask = new Task(taskName);
         newProject.addTask(newTask);
-        projects.push(newProject);
-        System.out.println(newProject.getStart().format(formatter)
+        projects.add(newProject);
+        StringBuilder sb = new StringBuilder();
+        sb.append(newProject.getStart().format(formatter)
                 + " " + newProject.getName()
                 + " " + newTask.getName());
+        System.out.println(sb);
     }
 
-    public static void stop() {
-        Project lastProject = projects.pop();
-        lastProject.setEnd(LocalDateTime.now());
-        System.out.println(lastProject.getEnd().format(formatter)
-                + " " + lastProject.getName());
+    public void stop() {
+        Project newProject = new Project("MWO");
+        Task newTask = new Task("task");
+        newProject.addTask(newTask);
+        newProject.setEnd(LocalDateTime.now());
+        projects.add(newProject);
+//        Project lastProject = projects.get(projects.size()-1);
+//        lastProject.setEnd(LocalDateTime.now());
+        StringBuilder sb = new StringBuilder();
+        sb.append(newProject.getStart().format(formatter)
+                + " | " + newProject.getName()
+                + " | " + newProject.getTasks().get(0).getName());
+        sb.insert(17, " --> " + newProject.getEnd().format(formatter));
+        System.out.println(sb);
     }
 
 
     public static void main(String[] args) {
-        start("MWO" , "task");
-        stop();
+        Demo demo = new Demo();
+
+
+        if (args.length == 5) {
+            if (args[0].equals("start") && args[1].equals("-p") && args[3].equals("-t")) {
+                demo.start(args[2], args[4]);
+            }
+        }
+
+        if (args.length == 1) {
+            if (args[0].equals("stop")) {
+                demo.stop();
+            }
+        }
 
     }
 }
