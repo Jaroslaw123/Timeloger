365544cfe5cba2ca6b9937ac1a41cf78c5f93372
lszydlow <szyd.luk@gmail.com>
2023-05-28 13:53:22 +0200

    dodanie continue

--- src/main/java/Help.java
+++ src/main/java/Help.java
@@ -5,9 +5,10 @@ public class Help {
         message = "To jest pomocy programu Tiemloger:" +
                 "start -p <nazwa projektu> -t <nazwa zadania> -rozpoczęcie nowego zadania," +
                 "stop - zakończenie bieżącego zadania," +
+                "continue + n - rozpoczęcie zadania o indeksie n," +
                 "last + n - wyświetlanie n ostatnich logów wraz z numerami indeksów," +
                 "list - wyświetlanie wszystkich logów," +
-                "report - generowanie raportu.";
+                "report - generowanie raportu z wybranych dat.";
     }
     public void displayHelpMessage() {
         System.out.println(message);
